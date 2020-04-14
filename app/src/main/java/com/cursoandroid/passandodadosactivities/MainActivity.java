2448208e9d1d0cb30bb9be341f0f3c1356599622
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-04-14 19:19:03 -0300

    Commit - Creating second Activity

--- app/src/main/java/com/cursoandroid/passandodadosactivities/MainActivity.java
+++ app/src/main/java/com/cursoandroid/passandodadosactivities/MainActivity.java
@@ -2,13 +2,33 @@ package com.cursoandroid.passandodadosactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentProviderClient;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonEnviar;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
     }
+
+
 }
