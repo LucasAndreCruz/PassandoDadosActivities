4b30ca5b945723a40e6dc9e863fd4b6bb1bac18d
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-04-14 19:36:23 -0300

    Commit - Instantiating object of type intent, and creating its methods for passing data with putExtra, and generating a bundle object to retrieve data with getIntent...

--- app/src/main/java/com/cursoandroid/passandodadosactivities/MainActivity.java
+++ app/src/main/java/com/cursoandroid/passandodadosactivities/MainActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.passandodadosactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentProviderClient;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                //Passar dados
+
+                intent.putExtra("nome", "Jamilton");
+                intent.putExtra("Idade", "30");
+
+                startActivity(intent);
 
             }
         });
--- app/src/main/java/com/cursoandroid/passandodadosactivities/SegundaActivity.java
+++ app/src/main/java/com/cursoandroid/passandodadosactivities/SegundaActivity.java
@@ -3,12 +3,24 @@ package com.cursoandroid.passandodadosactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome, textIdade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        textNome = findViewById(R.id.textNome);
+        textIdade = findViewById(R.id.textIdade);
+
+        //Recuperar os dados enviados
+
+        Bundle dados = getIntent().getExtras();
+
     }
 }
